c56bc1bf0b85250ad4953ee7ff3859a8fc142bef
avv-dev <vildan.abdullin@gmail.com>
2019-10-23 21:32:23 +0500

    1. Added ascending sorting. 2. Variable "list" renamed to "list_of_digits" in order to avoid conflicts.

--- 1seq.py
+++ 1seq.py
@@ -4,11 +4,12 @@
 # контроль ввода только одной цифры, а не числа (что соответствует заданию)
 
 list_length = int(input('Введите количество элементов: '))
-list = []
+list_of_digits = []
 for i in range(list_length):
     elem = input(f'Введите {i + 1}-й элемент: ')
     while not (elem.isdigit() and len(elem) == 1):
         elem = input('Элементом может быто только одна цифра! Попробуйте еще раз: ')
-    list.append(elem)
+    list_of_digits.append(elem)
     elem = ''
-print (list)
+list_of_digits.sort()
+print (list_of_digits)
